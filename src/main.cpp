8ca50cf27d8d7a6972e9ce3d0b5e7965fed8d307
GeorgeFlorian <georgeflorian12@gmail.com>
2021-08-26 15:05:59 +0300

    +

--- src/main.cpp
+++ src/main.cpp
@@ -110,22 +110,23 @@ void setup()
               request->send(200, "application/json", response);
             });
 
-  AsyncCallbackJsonWebHandler *handler = new AsyncCallbackJsonWebHandler("/post-message", [](AsyncWebServerRequest *request, JsonVariant &json)
-                                                                         {
-                                                                           StaticJsonDocument<200> data;
-                                                                           if (json.is<JsonArray>())
-                                                                           {
-                                                                             data = json.as<JsonArray>();
-                                                                           }
-                                                                           else if (json.is<JsonObject>())
-                                                                           {
-                                                                             data = json.as<JsonObject>();
-                                                                           }
-                                                                           String response;
-                                                                           serializeJson(data, response);
-                                                                           request->send(200, "application/json", response);
-                                                                           Serial.println(response);
-                                                                         });
+  AsyncCallbackJsonWebHandler *handler =
+      new AsyncCallbackJsonWebHandler("/post-message", [](AsyncWebServerRequest *request, JsonVariant &json)
+                                      {
+                                        StaticJsonDocument<200> data;
+                                        if (json.is<JsonArray>())
+                                        {
+                                          data = json.as<JsonArray>();
+                                        }
+                                        else if (json.is<JsonObject>())
+                                        {
+                                          data = json.as<JsonObject>();
+                                        }
+                                        String response;
+                                        serializeJson(data, response);
+                                        request->send(200, "application/json", response);
+                                        Serial.println(response);
+                                      });
   server.addHandler(handler);
 
   server.onNotFound(notFound);
