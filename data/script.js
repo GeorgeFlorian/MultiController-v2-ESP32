bbf868e9d105e9860104fda546b9ff31017da4f8
GeorgeFlorian <georgeflorian12@gmail.com>
2021-08-27 19:49:13 +0300

    Tested local JSON Server

--- data/script.js
+++ data/script.js
@@ -1,25 +1,25 @@
 const ROOT_URL = window.location.href;
 let conn_status = 0;
 
-window.onload = function () {
-  let form = document.getElementById("settings_form");
+// window.onload = function () {
+//   let form = document.getElementById("settings_form");
 
-  function processForm(e) {
-    if (e.preventDefault) e.preventDefault();
+//   function processForm(e) {
+//     if (e.preventDefault) e.preventDefault();
 
-    let formData = JSON.stringify(form).serializeArray();
-    console.log(formData);
+//     let formData = JSON.stringify(form).serializeArray();
+//     console.log(formData);
 
-    // You must return false to prevent the default form behavior
-    return false;
-  }
+//     // You must return false to prevent the default form behavior
+//     return false;
+//   }
 
-  if (form.attachEvent) {
-    form.attachEvent("submit", processForm);
-  } else {
-    form.addEventListener("submit", processForm);
-  }
-};
+//   if (form.attachEvent) {
+//     form.attachEvent("submit", processForm);
+//   } else {
+//     form.addEventListener("submit", processForm);
+//   }
+// };
 
 // create api handler in main.cc
 // create api handler in script.js
