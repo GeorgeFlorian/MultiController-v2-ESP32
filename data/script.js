10c6a347284a69fde57111eb29b54992426d3c5f
GeorgeFlorian <georgeflorian12@gmail.com>
2021-08-27 20:30:19 +0300

    +

--- data/script.js
+++ data/script.js
@@ -1,4 +1,5 @@
-const ROOT_URL = window.location.href;
+const ROOT_URL = "http://localhost:3000/"; // for testing with local json server
+// const ROOT_URL = window.location.href; // for production
 let conn_status = 0;
 
 // window.onload = function () {
@@ -24,58 +25,71 @@ let conn_status = 0;
 // create api handler in main.cc
 // create api handler in script.js
 
-// parse form data into json
-const getFormData = (form_id) => {
-  const form = document.getElementById(`${form_id}`);
-  return new FormData(form);
-};
+window.addEventListener("load", function () {
+  // parse form data into json
+  const getFormData = (form_id) => {
+    const form = document.getElementById(`${form_id}`);
+    console.log(form);
+    return new FormData(form);
+  };
 
-function toJSONstring(formData) {
-  var object = {};
-  formData.forEach((value, key) => {
-    // Reflect.has in favor of: object.hasOwnProperty(key)
-    if (!Reflect.has(object, key)) {
-      object[key] = value;
-      return;
-    }
-    if (!Array.isArray(object[key])) {
-      object[key] = [object[key]];
-    }
-    object[key].push(value);
-  });
-  console.log(JSON.stringify(object));
-  return JSON.stringify(object);
-}
+  let settings_form = getFormData("settings_form");
 
-// send/post json
-async function post_data(api_path, json_data) {
-  let post_url = ROOT_URL + api_path;
-  const response = await fetch(post_url, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: json_data,
-  });
-  //check
-  if (!response.ok) {
-    conn_status = 0;
-    const message = `An error has occured: ${response.status}`;
-    throw new Error(message);
+  function toJSONstring(formData) {
+    let object = {};
+    formData.forEach((value, key) => {
+      // Reflect.has in favor of: object.hasOwnProperty(key)
+      if (!Reflect.has(object, key)) {
+        object[key] = value;
+        return;
+      }
+      if (!Array.isArray(object[key])) {
+        object[key] = [object[key]];
+      }
+      object[key].push(value);
+    });
+    console.log(JSON.stringify(object));
+    return JSON.stringify(object);
   }
-  return await response.json();
-}
 
-function save_settings() {
-  //   let form = document.getElementById("settings_form");
-  let form = getFormData("settings_form");
-  let json_data = toJSONstring(form);
-  post_data("api/settings/change", json_data).then((send_response) => {
-    console.log(send_response);
-    conn_status = 1;
-    // logs to page
-    // get_settings() // update interface with newer settings
+  // send/post json
+  async function post_data(api_path, json_data) {
+    let post_url = ROOT_URL + api_path;
+    const response = await fetch(post_url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: json_data,
+    });
+    //check
+    if (!response.ok) {
+      conn_status = 0;
+      const message = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+    return await response.json();
+  }
+
+  function save_settings() {
+    //   let form = document.getElementById("settings_form");
+    let json_data = toJSONstring(settings_form);
+    post_data("api/settings/change", json_data).then((send_response) => {
+      console.log(send_response);
+      conn_status = 1;
+      // logs to page
+      // get_settings() // update interface with newer settings
+    });
+  }
+  // get json
+
+  // handle form submit
+  settings_form.addEventListener("submit", function (e) {
+    e.preventDefault(); // before the code
+    save_settings(form);
+
+    // Should be triggered on form submit
+    console.log("hi");
   });
-}
-// get json
+});
